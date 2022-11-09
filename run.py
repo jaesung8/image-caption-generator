3e1e752c7452f9af5b53eddceb81cfa59d003874
jaeseong2 <jaeseong9810@gmail.com>
2022-11-09 22:45:32 +0900

    feat(global): add run script

---
+++ run.py
@@ -0,0 +1,24 @@
+import datetime
+
+import click
+import pytz
+
+from src.predictor import Predictor
+from src.trainer import Trainer
+
+@click.command()
+def train():
+    hyperparameters = {
+        "learning_rate": 1e-4,
+        "gamma": 0.98,
+        "num_epochs": 100,
+        "epoch_interval": 10,
+    }
+
+    trainer = Trainer(hyperparameters)
+    trainer.train()
+
+
+@click.command()
+def predict():
+    pass
